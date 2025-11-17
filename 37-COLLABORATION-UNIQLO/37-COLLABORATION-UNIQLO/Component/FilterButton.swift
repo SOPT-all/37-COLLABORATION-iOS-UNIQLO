8dba423a4f68f0bcea832f6809229cf8052a19ca
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-11-17 23:48:24 +0900

    feat: #11 필터링 컴포넌트 구현 완

--- 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Component/FilterButton.swift
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Component/FilterButton.swift
@@ -1,5 +1,5 @@
 //
-//  filterButton.swift
+//  FilterButton.swift
 //  37-COLLABORATION-UNIQLO
 //
 //  Created by 정윤아 on 11/17/25.
@@ -24,9 +24,19 @@ enum FilterType: String, CaseIterable {
         }
     }
     
+    var icon: UIImage? {
+        switch self {
+        case .reset: return UIImage(named: "reset")
+        case .popularity: return UIImage(named: "drop")
+        case .category: return UIImage(named: "drop")
+        case .price: return UIImage(named: "drop")
+        case .color: return UIImage(named: "drop")
+        }
+    }
+    
 }
 
-final class filterButton : UIButton {
+final class FilterButton : UIButton {
     private let type: FilterType
     
     init(type: FilterType){
@@ -42,21 +52,30 @@ final class filterButton : UIButton {
     private func configure(){
         //버튼 스타일 공통
         clipsToBounds = true
-        layer.cornerRadius = 21
+        layer.cornerRadius = 15
         layer.borderWidth = 1
         layer.borderColor = UIColor(named: "gray300")?.cgColor
         
-        //타입에 맞는 이미지 적용
-//        setImage(type.buttonImage, for: .normal)
+        setTitle(type.title, for: .normal)
+        setTitleColor(.gray900, for: .normal)
+        titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 12)
+        setImage(type.icon, for: .normal)
         
         contentEdgeInsets = UIEdgeInsets(top: 6, left: 14, bottom: 6, right: 31)
         imageEdgeInsets = UIEdgeInsets(top: 9, left: 50, bottom: 9, right: 14)
         
         //아이콘을 오른쪽에 고정
         semanticContentAttribute = .forceRightToLeft
+        
+        // 전체 패딩
+        contentEdgeInsets = UIEdgeInsets(top: 6, left: 14, bottom: 6, right: 14)
+                
+        // 아이콘과 텍스트 간 간격 조정
+        imageEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
     }
     
-    
-    
-    
+}
+
+#Preview {
+    FilterButton(type: .popularity)
 }
