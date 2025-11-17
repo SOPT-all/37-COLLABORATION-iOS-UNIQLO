513d11763e28f66ec2b0829d41b191de22710a59
Yuna <166522604+laura-jung@users.noreply.github.com>
2025-11-17 22:13:32 +0900

    Update ViewController.swift

--- 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/ViewController.swift
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.backgroundcolor = .black
+        self.backgroundcolor = .white
         // Do any additional setup after loading the view.
     }
 }
