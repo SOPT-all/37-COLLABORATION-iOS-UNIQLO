c8a49d6aec99f63cff94f190d64dda6fec4e2dad
yeonee <leenayeon@sookmyung.ac.kr>
2025-11-18 22:17:38 +0900

    chore: #10 탭바 설정을 위한 뷰컨트롤러 설정

---
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Feature/Category/CategoryView.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryView.swift
+//  37-COLLABORATION-UNIQLO
+//
+//  Created by 이나연 on 11/18/25.
+//
+
+import UIKit
+
+final class CategoryView: BaseView {
+    
+}
---
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Global/CustomTabBar/EmptyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EmptyViewController.swift
+//  37-COLLABORATION-UNIQLO
+//
+//  Created by 이나연 on 11/18/25.
+//
+
+import UIKit
+
+final class EmptyViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
