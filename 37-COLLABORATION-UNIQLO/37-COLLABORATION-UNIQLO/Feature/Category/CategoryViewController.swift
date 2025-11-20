83c19b8cf2ad4c764d8fb6f7655d02085054194c
yeonee <leenayeon@sookmyung.ac.kr>
2025-11-21 01:58:20 +0900

    style: #32 사이드 아이템 테이블뷰 구현

--- 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Feature/Category/CategoryViewController.swift
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Feature/Category/CategoryViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 final class CategoryViewController: BaseViewController {
     
     private let rootView = CategoryView()
-
+    private let itemData = CategoryItemModel.mockData
+    private let outerData = OuterItemModel.mockData
+    
+    private var itemSelectedIndex: Int = 1
+    private var outerSelectedIndex: Int = 0
+    
     override func loadView() {
         view = rootView
     }
@@ -18,5 +23,40 @@ final class CategoryViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        setDelegate()
+        register()
     }
+    
+    override func setDelegate() {
+        rootView.sideTableView.delegate = self
+        rootView.sideTableView.dataSource = self
+    }
+    
+    private func register() {
+        rootView.sideTableView.register(SideTableViewCell.self, forCellReuseIdentifier: SideTableViewCell.identifier)
+    }
+}
+
+extension CategoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemSelectedIndex = indexPath.item
+        tableView.reloadData()
+        tableView.layoutIfNeeded()
+    }
+}
+
+extension CategoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SideTableViewCell.identifier) as? SideTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.bind(with: itemData[indexPath.row].item, isSelected: indexPath.row == itemSelectedIndex)
+        return cell
+    }
+    
 }
