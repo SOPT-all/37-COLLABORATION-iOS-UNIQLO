905401a7783752f02727d740d2e4b14acdbd249a
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-11-21 01:21:52 +0900

    feat: #28 view, viewController 기본세팅

--- 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Feature/ItemList/ItemListView.swift
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Feature/ItemList/ItemListView.swift
@@ -4,4 +4,27 @@
 //
 //  Created by 정윤아 on 11/21/25.
 //
+import UIKit
+import SnapKit
+import Then
 
+final class ItemListView: BaseView {
+    
+    private let categoryTabBarView = CategoryTabBarView()
+    private let resetButton = FilterButton(type: .reset)
+    private let popularityButton = FilterButton(type: .popularity)
+    private let categoryButton = FilterButton(type: .category)
+    private let priceButton = FilterButton(type: .price)
+    private let colorButton = FilterButton(type: .color)
+    private let topNavigationBar = TopNavigationBar()
+    private let customTabBarView = CustomTabBarView()
+    
+    override func setStyle() {
+    }
+    
+    override func setUI() {
+    }
+    
+    override func setLayout() {
+    }
+}
---
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Feature/ItemList/ItemListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ItemListViewController.swift
+//  37-COLLABORATION-UNIQLO
+//
+//  Created by 정윤아 on 11/21/25.
+//
+import UIKit
+import SnapKit
+import Then
+
+final class ItemListViewController: BaseViewController {
+    
+    
+    
+}
