17263fd8d8bd0e3d10a80f7dc81b010d29faec8c
yeonee <leenayeon@sookmyung.ac.kr>
2025-11-21 00:59:12 +0900

    feat: #35 기본 구조 세팅

---
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Network/EndPoint/EndPoint.swift
@@ -0,0 +1,12 @@
+//
+//  EndPoint.swift
+//  37-COLLABORATION-UNIQLO
+//
+//  Created by 이나연 on 11/20/25.
+//
+
+protocol EndPoint {
+    /// ex) /api/v1/products/{productId}/details
+    ///
+    /// basePath - /api/v1
+}
