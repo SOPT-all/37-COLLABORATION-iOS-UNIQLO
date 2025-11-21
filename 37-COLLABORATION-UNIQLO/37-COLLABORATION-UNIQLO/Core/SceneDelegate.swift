70219036b5b28386b4673782affa801bf7b9c7b5
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-11-21 21:48:40 +0900

    refactor: 프리뷰 삭제

--- 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Core/SceneDelegate.swift
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Core/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = UINavigationController(rootViewController: CustomTabBarViewController())
+        let vc = UINavigationController(rootViewController: ItemListViewController())
         window.rootViewController = vc
         self.window = window
         window.makeKeyAndVisible()
