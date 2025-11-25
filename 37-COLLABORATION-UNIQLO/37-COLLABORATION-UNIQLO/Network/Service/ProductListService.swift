a589cbf1c300e7d3f4d48d64753b27049d6a1346
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-11-25 22:50:07 +0900

    feat: responseDTO, service 세팅

--- 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Network/Service/ProductListService.swift
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Network/Service/ProductListService.swift
@@ -5,3 +5,22 @@
 //  Created by 이나연 on 11/22/25.
 //
 
+protocol ProductListService {
+    func getProductList() async throws -> [Item]
+}
+
+final class DefaultProductListService: ProductListService {
+    let network = NetworkService.shared
+    
+    func getProductList() async throws -> [Item] {
+        do {
+            let list: [ProductListDTO] = try await network.request(endPoint: .products)
+            return list.map {
+                $0.toItem()
+            }
+        } catch {
+            print("네트워크 요청 에러: \(error.localizedDescription)")
+            throw error
+        }
+    }
+}
