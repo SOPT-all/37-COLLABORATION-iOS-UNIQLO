5b7e0829c9dee0d49b4e8b02b20099eeea535b3f
soo127 <ss_127@naver.com>
2025-11-26 00:04:19 +0900

    feat: #40 제품 상세 조회

---
+++ 37-COLLABORATION-UNIQLO/37-COLLABORATION-UNIQLO/Feature/DetailPage/Models/ProductInfoResponse.swift
@@ -0,0 +1,18 @@
+//
+//  ProductInfoResponse.swift
+//  37-COLLABORATION-UNIQLO
+//
+//  Created by 이상수 on 11/25/25.
+//
+
+import UIKit
+
+struct ProductInfoResponse: Decodable {
+    let productId: String
+    let productImageUrl: [URL]
+    let colorCode: [String]
+    let color: [String: String]
+    let name: String
+    let originPrice: String
+    let starAverage: Float
+}
